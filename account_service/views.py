f2e5c65e214d48fb4ff823217d8fa524ccd115cb
Abram <israelvictory87@gmail.com>
2022-08-07 21:46:14 +0100

    Created LoginAPIView and RefreshLoginAPIView

--- account_service/views.py
+++ account_service/views.py
@@ -1,11 +1,16 @@
 # Rest Framework Imports
-from rest_framework import status, views
+from rest_framework import status, views, permissions
 from rest_framework.response import Response
 from rest_framework.request import Request
 
+# SimpleJWT Imports
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
+
 # Account Service Imports
 from account_service.serializers import (
-    RegisterUserSerializer
+    RegisterUserSerializer,
+    UserLoginObtainPairSerializer
 )
 
 # Third Party Imports
@@ -14,6 +19,7 @@ from rest_api_payload import success_response, error_response
 
 class RegisterAPIView(views.APIView):
     serializer_class = RegisterUserSerializer
+    permission_classes = [permissions.AllowAny]
     
     def post(self, request:Request) -> Response:
         serializer = RegisterUserSerializer(data=request.data)
@@ -31,3 +37,16 @@ class RegisterAPIView(views.APIView):
             status="error", message=serializer.errors
         )
         return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginAPIView(TokenObtainPairView):
+    """Inherits TokenObtainPairView from rest_framework simplejwt"""
+
+    serializer_class = UserLoginObtainPairSerializer
+
+
+
+class RefreshLoginAPIView(TokenRefreshView):
+    """Inherits TokenRefreshView from rest_framework simplejwt"""
+
+    serializer_class = TokenRefreshSerializer
