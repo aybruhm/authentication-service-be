bf3642561668bbcb011eb600c67274a7d61f0fc8
Abram <israelvictory87@gmail.com>
2022-08-07 21:29:57 +0100

    Created send_email_to_user and send_reset_password_email_to_user functionality

--- account_service/services/emails/users.py
+++ account_service/services/emails/users.py
@@ -1,30 +1,59 @@
-# Typing Imports
-from typing import Dict, List
-
 # Django Imports
-from django.contrib.auth import get_user_model
-from django.conf import settings
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpRequest
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
+
+# Types Imports
+from typing import NoReturn
+
+# Accounts Imports
+from account_service.models import AccountUser
 
 
-User = get_user_model()
 
-
-def send_html_email_to_user(
-    to_list: List[str, str],
-    subject: str,
-    template_name,
-    context: Dict,
-    sender=settings.DEFAULT_FROM_EMAIL,
-):
+def send_email_to_user(request: HttpRequest, user:AccountUser, uid:str, token:str) -> NoReturn:
+    
+    current_site = get_current_site(request)
+    mail_subject = '[Loopscentral]: Verify Your Email'
+    
+    email_context = {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': uid,
+        'token': token,
+    }
+    
     # Parse html to string
-    email_html_message = render_to_string(template_name, context)
-    # Initialize a single email message which can be send to multiple recipients
-    msg = EmailMultiAlternatives(
-        subject=subject, body=email_html_message, from_email=sender, to=to_list
-    )
-    # Changes content type to text/html
-    msg.content_subtype = "html"
-    # Sends email and fail silently
-    return msg.send()
+    html_content = render_to_string('emails/verify-email-template.html', email_context)
+    text_content = strip_tags(html_content)
+
+    # # Initialize a single email message which can be send to multiple recipients
+    msg = EmailMultiAlternatives(mail_subject, text_content, settings.EMAIL_HOST_USER, [user.email])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
+
+
+def send_reset_password_email_to_user(request: HttpRequest, user:AccountUser, uid:str, token:str) -> NoReturn:
+    
+    current_site = get_current_site(request)
+    mail_subject = '[Loopscentral]: Reset Your Password'
+    
+    email_context = {
+        'user': user,
+        'domain': current_site.domain,
+        'uid': uid,
+        'token': token,
+    }
+    
+    # Parse html to string
+    html_content = render_to_string('emails/reset-password-email-template.html', email_context)
+    text_content = strip_tags(html_content)
+    
+    # # Initialize a single email message which can be send to multiple recipients
+    msg = EmailMultiAlternatives(mail_subject, text_content, settings.EMAIL_HOST_USER, [user.email])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
