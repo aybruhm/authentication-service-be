02b8e69cfcdad9acaeae68713730ff9e03b1158f
Abram <israelvictory87@gmail.com>
2022-08-07 21:40:23 +0100

    Created RegisterAPIVIew

--- account_service/views.py
+++ account_service/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# Rest Framework Imports
+from rest_framework import status, views
+from rest_framework.response import Response
+from rest_framework.request import Request
 
-# Create your views here.
+# Account Service Imports
+from account_service.serializers import (
+    RegisterUserSerializer
+)
+
+# Third Party Imports
+from rest_api_payload import success_response, error_response
+
+
+class RegisterAPIView(views.APIView):
+    serializer_class = RegisterUserSerializer
+    
+    def post(self, request:Request) -> Response:
+        serializer = RegisterUserSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            
+            payload = success_response(
+                status="success", message="User created",
+                data=serializer.data
+            )
+            return Response(data=payload, status=status.HTTP_201_CREATED)
+        
+        payload = error_response(
+            status="error", message=serializer.errors
+        )
+        return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
