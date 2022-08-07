53c1070ee69bd4f676254a676998780af038e714
Abram <israelvictory87@gmail.com>
2022-08-07 22:55:56 +0100

    Integrated drf-yasg

--- core/config/base.py
+++ core/config/base.py
@@ -50,6 +50,7 @@ THIRD_PARTY_APPS = [
     "rest_framework.authtoken",
     "rest_framework_simplejwt",
     "corsheaders",
+    "drf_yasg",
 ]
 
 INSTALLED_APPS = LOCAL_APPS + OWN_APPS + THIRD_PARTY_APPS
