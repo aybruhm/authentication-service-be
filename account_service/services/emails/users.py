a3a9f3a7f089988c9b6f1e35a3fda108ba8a8aa7
Abram <israelvictory87@gmail.com>
2022-08-07 21:23:58 +0100

    Created send_html_email_to_user email service

---
+++ account_service/services/emails/users.py
@@ -0,0 +1,30 @@
+# Typing Imports
+from typing import Dict, List
+
+# Django Imports
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+
+User = get_user_model()
+
+
+def send_html_email_to_user(
+    to_list: List[str, str],
+    subject: str,
+    template_name,
+    context: Dict,
+    sender=settings.DEFAULT_FROM_EMAIL,
+):
+    # Parse html to string
+    email_html_message = render_to_string(template_name, context)
+    # Initialize a single email message which can be send to multiple recipients
+    msg = EmailMultiAlternatives(
+        subject=subject, body=email_html_message, from_email=sender, to=to_list
+    )
+    # Changes content type to text/html
+    msg.content_subtype = "html"
+    # Sends email and fail silently
+    return msg.send()
