27ea035d7df46887afb089caf3dba606cf27fc9a
Abram <israelvictory87@gmail.com>
2022-08-07 22:57:20 +0100

    Update admin.py

--- account_service/admin.py
+++ account_service/admin.py
@@ -3,13 +3,13 @@ from django.contrib import admin
 from django.conf import settings
 
 # Account Service Imports
-from account_service.models import User
+from account_service.models import AccountUser
 
 
 # register user if set in the settings, otherwise don't.
 if settings.REGISTER_USER_MODEL:
     
-    @admin.register(User)
-    class UserAdmin(admin.ModelAdmin):
+    @admin.register(AccountUser)
+    class AccountUserAdmin(admin.ModelAdmin):
         list_display = ("id", "firstname", "lastname", "email", "username")
         list_filter = ("id", "email", "username")
