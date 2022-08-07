2d970ad9acaaf478aca5d0d827663f8edf2a0622
Abram <israelvictory87@gmail.com>
2022-08-07 22:56:09 +0100

    Created url routes for api documentation

--- core/urls.py
+++ core/urls.py
@@ -1,19 +1,34 @@
-"""core URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Django Imports
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
-urlpatterns = [path("admin/", admin.site.urls)]
+# DRF YASG Imports
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Rest Framework Imports
+from rest_framework import permissions
+
+
+# Schema Definition
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Loopscentral Internal Account Service API",
+      default_version='v1',
+      description="Authentication service backend apis for loopscentral platform",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="loopscentraltech@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/", include("account_service.urls"))
+]
+
+# API Documentation APIs
+urlpatterns + re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+urlpatterns + re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+urlpatterns + re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
