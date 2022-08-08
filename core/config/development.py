e6ec45de686cbf2f8e439045beccde62989fb6ce
Abram <israelvictory87@gmail.com>
2022-08-08 17:43:42 +0100

    Update development

--- core/config/development.py
+++ core/config/development.py
@@ -22,10 +22,3 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
-# SMTP Definition
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.mailtrap.io"
-EMAIL_HOST_USER = config("YOUR_USER")
-EMAIL_HOST_PASSWORD = config("YOUR_PASSWORD")
-EMAIL_PORT = "2525"
