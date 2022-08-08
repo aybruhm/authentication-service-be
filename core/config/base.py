ab5ab206a856ff5b330babbafd065045a12cf4a4
Abram <israelvictory87@gmail.com>
2022-08-08 15:34:13 +0100

    Update config

--- core/config/base.py
+++ core/config/base.py
@@ -18,7 +18,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Runtime Environment
 RUNTIME_ENVIRON = "core.config.development"  # when serving to production, change to '.production'
@@ -133,13 +133,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
-}
 
 
 # Password validation
--- core/config/development.py
+++ core/config/development.py
@@ -5,3 +5,27 @@ from core.config.base import * #noqa
 DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = ["127.0.0.1"]
+
+
+THIRD_PARTY_APPS += []
+
+# Middleware Definition
+MIDDLEWARE += []
+
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+# SMTP Definition
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.mailtrap.io"
+EMAIL_HOST_USER = config("YOUR_USER")
+EMAIL_HOST_PASSWORD = config("YOUR_PASSWORD")
+EMAIL_PORT = "2525"
