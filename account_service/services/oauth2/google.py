25099a150f84dc6dfca1113de7d76dcc9f7c667e
Abram <israelvictory87@gmail.com>
2022-08-12 11:28:56 +0100

    Created google_validate_id_token service

--- account_service/services/oauth2/google.py
+++ account_service/services/oauth2/google.py
@@ -14,21 +14,23 @@ async def google_validate_id_token(*, id_token: str) -> bool:
     Authenticate this request by comparing the aud (short for audience) 
     from the Google response with the GOOGLE_OAUTH2_CLIENT_ID
     """
-    response = await httpx.get(
-        GOOGLE_ID_TOKEN_INFO_URL,
-        params={'id_token': id_token}
-    )
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            GOOGLE_ID_TOKEN_INFO_URL,
+            params={'id_token': id_token}
+        )
 
-    # Check is response is not "OK"
-    if not response.ok:
-        raise ValidationError('id_token is invalid.')
+        # Check is response is not "OK"
+        if not response.ok:
+            raise ValidationError('id_token is invalid.')
 
-    # Get audience data
-    audience = response.json()['aud']
+        # Get audience data
+        audience = response.json()['aud']
 
-    # Compare audience with that of the settings 
-    # configured oauth2 google client id
-    if audience != settings.GOOGLE_OAUTH2_CLIENT_ID:
-        raise ValidationError('Invalid audience.')
+        # Compare audience with that of the settings 
+        # configured oauth2 google client id
+        if audience != settings.GOOGLE_OAUTH2_CLIENT_ID:
+            raise ValidationError('Invalid audience.')
 
-    return True
+        return True
