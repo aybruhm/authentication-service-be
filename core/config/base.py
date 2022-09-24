0ef114a833b21e26c7069d520a4ba8c7dd8b5a9f
Abram <israelvictory87@gmail.com>
2022-09-24 22:18:38 +0100

    Changed email backend to console

--- core/config/base.py
+++ core/config/base.py
@@ -176,12 +176,13 @@ AUTH_USER_MODEL = "authentication_service.AccountUser"
 REGISTER_USER_MODEL = True
 
 # Email Backend Definition
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "admin@gmail.com"
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "admin@gmail.com"
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = 'noreply@admin.com'
 
