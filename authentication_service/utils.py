87a16bc41df1059924edb44b147269ca83b847bc
Abram <israelvictory87@gmail.com>
2022-09-26 22:45:33 +0100

    Refactor utils -> get_inactive and get_active user functions

--- authentication_service/utils.py
+++ authentication_service/utils.py
@@ -1,36 +1,35 @@
 # Rest Framework Imports
 from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework import status
 
 # Account Service Imports
 from authentication_service.models import AccountUser
 
-# Third Party Imports
-from rest_api_payload import error_response
-
 
 def get_inactive_user(request:Request, email:str):
+    """
+    This function returns the user with the given email if the user is inactive
     
-    try:
-        user = AccountUser.objects.filter(email=email).first()
-        return user
-    except (AccountUser.DoesNotExist, Exception):
-        payload = error_response(
-            status="error",
-            message="User does not exist!"
-        )
-        return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
+    :param request: This is the request object that is passed to the view
+    :type request: Request
+    :param email: The email address of the user you want to get
+    :type email: str
+    :return: The user object is being returned.
+    """
+    user = AccountUser.objects.filter(email=email, is_active=False).first()
+    return user
     
 
 def get_active_user(request:Request, email:str):
+    """
+    This function returns an active user with the given email address
     
-    try:
-        user = AccountUser.objects.filter(email=email, is_active=True).first()
-        return user
-    except (AccountUser.DoesNotExist, Exception):
-        payload = error_response(
-            status="error",
-            message="User does not exist!"
-        )
-        return Response(data=payload, status=status.HTTP_400_BAD_REQUEST)
+    :param request: This is the request object that is passed to the view
+    :type request: Request
+    :param email: The email address of the user
+    :type email: str
+    :return: The user object
+    """
+    
+    user = AccountUser.objects.filter(email=email, is_active=True).first()
+    return user
+    
