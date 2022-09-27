39ec8ecc9f231dc60b32f378f4d315e0ef318392
Abram <israelvictory87@gmail.com>
2022-09-27 22:11:02 +0100

    Added extra validation for edit methods

--- authentication_service/permissions.py
+++ authentication_service/permissions.py
@@ -1,4 +1,7 @@
+# Typing Imports
 from typing import Any
+
+# Rest Framework Imports
 from rest_framework import permissions
 from rest_framework.request import Request
 
@@ -6,13 +9,13 @@ from rest_framework.request import Request
 class CanSuspendUserPermission(permissions.BasePermission):
     
     message = "You can not suspend a user if you are not our staff!"
-    edit_methods = ("PUT", "PATCH")
+    edit_methods = ("PUT",)
     
     def has_permission(self, request:Request, view:Any) -> bool:
         if request.user.is_authenticated and request.user.is_staff:
             return True
         
     def has_object_permission(self, request:Request, view:Any) -> bool:
-        if request.user.has_perm("user.can_suspend_user"):
+        if request.method not in self.edit_methods and request.user.has_perm("user.can_suspend_user"):
             return True
         return False
