906b0b4f3214791f0a70cc33dead63d43bb7a256
Abram <israelvictory87@gmail.com>
2022-10-03 21:25:27 +0100

    Minor refactor: update allowed_hosts

--- core/config/development.py
+++ core/config/development.py
@@ -4,7 +4,7 @@ from core.config.base import * #noqa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0"]
 
 
 THIRD_PARTY_APPS += []
